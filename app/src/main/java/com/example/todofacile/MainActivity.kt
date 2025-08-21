ede46d944b29101870d41963cf9cd01bc305793e
rodofrodo <rodofrodo708@gmail.com>
2025-08-21 21:06:50 +0200

    Removed comment

--- app/src/main/java/com/example/todofacile/MainActivity.kt
+++ app/src/main/java/com/example/todofacile/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    // xd
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
