4995ec0d4be55a759bdcb82a900f562528d6c00c
rodofrodo <rodofrodo708@gmail.com>
2025-08-28 20:55:35 +0200

    Added some code to show current date

--- app/src/main/java/com/example/todofacile/MainActivity.kt
+++ app/src/main/java/com/example/todofacile/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.todofacile
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+    // global control init
+    private lateinit var dateLbl: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
@@ -18,5 +26,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // init
+        dateLbl = findViewById(R.id.dateLbl)
+        showCurrentDate()
+    }
+
+    private fun showCurrentDate() {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+        val customFormatted = current.format(formatter)
+        dateLbl.text = customFormatted
     }
 }
