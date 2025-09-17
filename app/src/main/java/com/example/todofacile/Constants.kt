14f2fd6e70a88352d38b3119d7771436ecbfc761
rodofrodo <rodofrodo708@gmail.com>
2025-09-17 22:29:01 +0200

    Added some functions to add a listener

--- app/src/main/java/com/example/todofacile/Constants.kt
+++ app/src/main/java/com/example/todofacile/Constants.kt
@@ -2,4 +2,14 @@ package com.example.todofacile
 
 object Constants {
     const val BLOCKING_WORD: String = "!NOT"
+    private var members = mutableListOf<AddTaskListener>()
+
+    fun addMember(atl: AddTaskListener) {
+        members.add(atl)
+    }
+
+    fun addInstance(titleText: String, a1: String, c1: Int, a2: String, c2: Int, a3: String, c3: Int) {
+        for (member in members)
+            member.addInstance(titleText, a1, c1, a2, c2, a3, c3)
+    }
 }
